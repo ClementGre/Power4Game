d71e55157b6ce3df3894cbe12d824232b021ded2
Clément Grennerat <clement.grennerat@free.fr>
2024-06-14 13:55:13 +0200

    Adding docstrings

--- src/ui/app_buttons_frame.py
+++ src/ui/app_buttons_frame.py
@@ -14,13 +14,22 @@ class AppButtonsFrame(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Crée les widgets du panneau d'application.
+        """
         self.buttons.append(tk.Button(self, text="FullScreen", command=self.fullscreen))
         self.buttons[-1].pack(side=tk.LEFT, padx=10, pady=5)
 
     def fullscreen(self):
+        """
+        Active le mode plein écran.
+        """
         self.master.master.attributes("-fullscreen", True)
         self.buttons[0].config(text="Exit FullScreen", command=self.exit_fullscreen)
 
     def exit_fullscreen(self):
+        """
+        Désactive le mode plein écran.
+        """
         self.master.master.attributes("-fullscreen", False)
         self.buttons[0].config(text="FullScreen", command=self.fullscreen)
--- src/ui/game_buttons_frame.py
+++ src/ui/game_buttons_frame.py
@@ -14,13 +14,24 @@ class GameButtonsFrame(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Crée les widgets du panneau de jeu.
+        """
         self.buttons.append(tk.Button(self, text="Resign", bg=bg, command=self.master.resign))
         self.buttons[-1].pack(side=tk.RIGHT, padx=10, pady=5)
         self.buttons.append(tk.Label(self, text="Time: 0.00", bg=bg, fg="#303030", font=("Courier", 16)))
         self.buttons[-1].pack(side=tk.RIGHT, padx=10, pady=5)
 
     def game_ended(self):
+        """
+        Met à jour les boutons lorsque la partie est terminée.
+        """
         self.buttons[0].config(text="Quit", command=self.master.master.end_game)
 
     def update_time(self, time):
+        """
+        Met à jour le temps affiché.
+        :param time: Nombre de millisecondes écoulées
+        :type time: int
+        """
         self.buttons[1].config(text=format_time_ms(time))
